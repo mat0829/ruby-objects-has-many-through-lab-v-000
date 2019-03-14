937c19856c431fb4d8b918d1a189b5a8079c3051
Mat Anderson <matandersonlv@hotmail.com>
2019-03-14 19:49:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,11 +6,15 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
   
   def self.all
     @@all
   end
   
+  def new_song(name, genre)
+    
+  end
   
 end
