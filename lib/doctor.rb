f735273e9f8a3dda50142bc78e8e3c011d3b49ed
Mat Anderson <matandersonlv@hotmail.com>
2019-03-14 20:53:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select {|appointment| Appointment.doctor == self}
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
 end
