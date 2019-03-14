79c5a64512a9676de327dacae5eb427544a052f4
Mat Anderson <matandersonlv@hotmail.com>
2019-03-14 21:07:29 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,7 +21,7 @@ class Doctor
   end
   
   def patients
-    Appointment.all.select {|appointment| appointment.patient == self}.eollect {|appointment| appointment.patient}
+    Appointment.all.select {|appointment| appointment.patient == self}.collect {|appointment| appointment.patient}
   end
   
 end
